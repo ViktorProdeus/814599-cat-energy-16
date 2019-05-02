a51939f239cd64da4f023b46c402c2de0d6aa2f9
Viktor <viktorprodeus@gmail.com>
2019-05-02 23:59:15 +0300

    внес изменение в js

--- source/js/toggle-menu.js
+++ source/js/toggle-menu.js
@@ -5,7 +5,6 @@ mainNav.classList.remove('main-nav--no-js');
 toogleMenu.classList.remove('page-header__toggle--no-js');
 
 toogleMenu.addEventListener('click', function () {
-  // toogleMenu.classList.toggle('page-header__toggle--closed');
   toogleMenu.classList.toggle('page-header__toggle--open');
   mainNav.classList.toggle('main-nav--open');
 });
