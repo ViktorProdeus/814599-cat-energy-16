ecb0d7cfba95b787981917dcdeea475fcef51351
ViktorProdeus <42042470+ViktorProdeus@users.noreply.github.com>
2021-06-05 23:55:38 +0300

    Revert "deploy"

---
+++ source/js/toggle-menu.js
@@ -0,0 +1,10 @@
+var toogleMenu = document.querySelector('.page-header__toggle');
+var mainNav = document.querySelector('.main-nav');
+
+mainNav.classList.remove('main-nav--no-js');
+toogleMenu.classList.remove('page-header__toggle--no-js');
+
+toogleMenu.addEventListener('click', function () {
+  toogleMenu.classList.toggle('page-header__toggle--open');
+  mainNav.classList.toggle('main-nav--open');
+});
